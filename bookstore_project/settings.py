46ca7c97f324ad5247ed860ffd760041bef358a9
bezalel001 <mickeybezalel@gmail.com>
2020-02-12 05:01:33 +0100

    added file/image uploads

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -144,6 +144,10 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
