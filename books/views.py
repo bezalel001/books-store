3a94405fe1eba34e65f0c2400cbed4b2708f9d3f
bezalel001 <mickeybezalel@gmail.com>
2020-02-14 12:17:35 +0100

    added a basic search filter

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 
@@ -17,3 +18,16 @@ class BookDetailView(LoginRequiredMixin,  PermissionRequiredMixin, DetailView):
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
