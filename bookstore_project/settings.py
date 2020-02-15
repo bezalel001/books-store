9a25dfd29a4fb638387a59cd5067ebb8cddb04a1
bezalel001 <mickeybezalel@gmail.com>
2020-02-15 14:40:22 +0100

    allowed_hosts update

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['vast-woodland-09193.herokuapp.com/',
+ALLOWED_HOSTS = ['vast-woodland-09193.herokuapp.com',
                  'localhost', '127.0.0.1']
 
 
